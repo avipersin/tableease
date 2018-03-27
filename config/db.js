badcf68432646b0511043e949f5210cf74afe33e
avipersin <avi.persin@gmail.com>
2018-03-27 16:37:09 -0400

    fixed laggout issue from database using a pool

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+
+const connection = mysql.createPool({
     host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'bfdecc39479008',
     password: 'd9d922b1',
     database: 'heroku_150cb394bbf6303'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-});
 
 module.exports = connection;
 
